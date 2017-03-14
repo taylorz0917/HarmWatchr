c187c34f597155bbca2fedab242d2b93c7e7cf36
Ryan Lieblein <Ryan Lieblein>
2017-03-13 22:45:42 -0400

    locate: Directs user to Google Maps... added calling functionality for call and ems

--- app/src/main/java/com/ufl/harmwatchr/harmwatcher/Status.java
+++ app/src/main/java/com/ufl/harmwatchr/harmwatcher/Status.java
@@ -2,10 +2,13 @@ package com.ufl.harmwatchr.harmwatcher;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.content.pm.PackageManager;
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 
 public class Status extends AppCompatActivity {
 
@@ -25,6 +28,7 @@ public class Status extends AppCompatActivity {
         final Button activity_peopleLink = (Button) findViewById(R.id.back);
         final Button locate = (Button) findViewById(R.id.location);
         final Button call = (Button) findViewById(R.id.call);
+        final Button ems = (Button) findViewById(R.id.ems);
 
 
         //If Back is pressed. (DONE)
@@ -43,37 +47,46 @@ public class Status extends AppCompatActivity {
         locate.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //Pull location information
-                //Open location in maps application
+                //input latitude, longitude from database
+                String uri = String.format("geo:%f,%f",59.915494, 30.409456);
+                Intent locate_Intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                Status.this.startActivity(locate_Intent);
             }
         });
+
+
 
         //If Call is pressed. (In Progress.)
         call.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                /*
                 //Retreive watchee's phone number
-                //Dial phone number
-
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
                 callIntent.setData(Uri.parse("tel:0377778888"));
 
                 if (ActivityCompat.checkSelfPermission(Status.this,
                         Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                     return;
                 }
-                startActivity(callIntent);
-                */
+                Status.this.startActivity(callIntent);
             }
 
         });
 
-        /*If EMS is pressed. (In Progress.)
-        ems.setOnClickListener(new View.OnClickListener(){
+        //If ems is pressed. (In Progress.)
+       /* ems.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //Call EMS number
+
+                Intent emsIntent = new Intent(Intent.ACTION_DIAL);
+                emsIntent.setData(Uri.parse("tel:911"));
+
+                if (ActivityCompat.checkSelfPermission(Status.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(emsIntent);
+
             }
 
         });*/
